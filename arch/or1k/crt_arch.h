65dbaddb25c8abe9e31d62b2817ac86c35066b07
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-06-01 22:29:01 +0300

    or1k: fix crt_arch.h
    our l.andi doesn't sign-extend and the stack pointer
    was passed in r4 instead of r3 to __cstart().

--- arch/or1k/crt_arch.h
+++ arch/or1k/crt_arch.h
@@ -2,10 +2,10 @@ __asm__("\
 .global _start \n\
 .align  4 \n\
 _start: \n\
-	l.ori r2, r0, 0 \n\
-	l.ori r4, r1, 0 \n\
-	l.andi r1, r1, -8 \n\
+	l.ori r3, r1, 0 \n\
+	l.addi r2, r0, -8 \n\
+	l.and r1, r1, r2 \n\
 	l.addi r1, r1, -8 \n\
 	l.jal __cstart \n\
-	 l.nop \n\
+	 l.ori r2, r0, 0 \n\
 ");

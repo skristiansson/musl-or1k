71e521ac28f7f625a80838fd889a92b98c59fe00
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-07-16 01:54:37 +0300

    or1k: fix thread pointer offset
    The +/- 4 was to account for the TCB, but __set_thread_area()
    is called like this:
    __set_thread_area(TP_ADJ(tls - sizeof(struct pthread)))
    so, the offset by 4 is not correct.

--- arch/or1k/pthread_arch.h
+++ arch/or1k/pthread_arch.h
@@ -7,11 +7,11 @@ static inline struct pthread *__pthread_self()
 #else
 	register char *tp __asm__("r10");
 #endif
-	return (struct pthread *) (tp - 4 - sizeof(struct pthread));
+	return (struct pthread *) (tp - sizeof(struct pthread));
 }
 
 #define TLS_ABOVE_TP
-#define TP_ADJ(p) ((char *)(p) + sizeof(struct pthread) + 4)
+#define TP_ADJ(p) ((char *)(p) + sizeof(struct pthread))
 
 /* word-offset to 'pc' in mcontext_t */
 #define CANCEL_REG_IP 32
